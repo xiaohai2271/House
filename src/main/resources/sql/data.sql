0671f18d334759cb3aa6f29fc606642691b29a8c
禾几海 <zh56462271@qq.com>
2021-06-05 14:00:41 +0800

    使用H2做单元测试

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,2 @@
-truncate user;
-insert into user (id, email, password) value (1, "a@celess.cn", "123456")
+INSERT INTO user (id, email, password)
+VALUES (1, 'a@celess.cn', '123456');
--- src/test/java/cn/celess/house/BaseTest.java
+++ src/test/java/cn/celess/house/BaseTest.java
@@ -1,12 +1,14 @@
 package cn.celess.house;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * @author : xiaohai
  * @date : 2019/08/27 20:35
  */
 @SpringBootTest
+@ActiveProfiles("test")
 public class BaseTest {
-    // just to provide test environment
 }
