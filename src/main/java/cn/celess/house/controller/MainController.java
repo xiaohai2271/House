d1c140cf53e79922e2305fbe3607ab4aa9238879
禾几海 <zh56462271@qq.com>
2021-07-25 21:24:37 +0800

    处理404响应

--- src/main/java/cn/celess/house/controller/MainController.java
+++ src/main/java/cn/celess/house/controller/MainController.java
@@ -1,8 +1,15 @@
 package cn.celess.house.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author: 小海
@@ -11,10 +18,24 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 @Slf4j
-public class MainController {
+public class MainController extends BasicErrorController {
 
-    @GetMapping({"/", "${server.error.path:${error.path:/error}}"})
+    public MainController(ServerProperties serverProperties) {
+        super(new DefaultErrorAttributes(), serverProperties.getError());
+    }
+
+    @GetMapping({"/"})
     public String todo() {
         return "forward:/index.html";
     }
+
+    /**
+     * 覆盖默认的HTML响应
+     */
+    @Override
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_OK);
+        return new ModelAndView("index.html");
+    }
+
 }
