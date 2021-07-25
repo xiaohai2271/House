7f9eff15ce8fb3f10f5722ec867f54a4c5c460d8
禾几海 <zh56462271@qq.com>
2021-07-25 18:44:47 +0800

    fix: id

--- client/web/src/app/modules/page/utils/Menu.ts
+++ client/web/src/app/modules/page/utils/Menu.ts
@@ -14,8 +14,8 @@ export declare class MenuID {
 }
 
 export const MENU: { [name: string]: MenuID } = {
-  all: {id: 1, name: '全部'},
-  plan: {id: 2, name: '计划'},
-  done: {id: 3, name: '已完成'},
-  task: {id: 4, name: '任务'}
+  all: {id: -1, name: '全部'},
+  plan: {id: -2, name: '计划'},
+  done: {id: -3, name: '已完成'},
+  task: {id: -4, name: '任务'}
 }
